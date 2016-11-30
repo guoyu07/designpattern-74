37e7661b4b31c16aa4b42e1f4c9a6826106b462e
lpwang <13889522960@163.com>
2016-12-01 00:14:41 +0800

    edit 1

--- designpattern/src/my/designpattern/create/SinglePattern.java
+++ designpattern/src/my/designpattern/create/SinglePattern.java
@@ -1,5 +1,14 @@
 package my.designpattern.create;
 
 public class SinglePattern {
-
+	
+	// 饿汉式单利模式
+	private static final SinglePattern SINGLEPATTERN = new SinglePattern();
+	
+	private SinglePattern(){}
+	
+	public static SinglePattern getInstance(){
+		return SinglePattern.SINGLEPATTERN;
+	}
+	
 }
